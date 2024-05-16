dc2a74a53a123f5e68c3a17a1585b5574d239b3d
Gustavo Manquepillán <gustavomanquepillan@gmail.com>
2024-05-15 22:18:32 -0400

    Subiendo proyecto OnlyFlans Hito_3 Punto_3

--- Proyectos/Proyecto/onlyflans/web/views.py
+++ Proyectos/Proyecto/onlyflans/web/views.py
@@ -1,11 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render 
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Flan, ContactForm
+from .forms import ContactFormForm
 # Create your views here.
 def indice(request):
-    return render(request, 'index.html')
-
-def bienvenido(request):
-    return render(request, 'welcome.html')
+    public_flans = Flan.objects.filter(is_private=False)
+    context = {
+        'public_flans': public_flans
+    }
+    return render(request, 'index.html', context)
 
 def acerca(request):
     return render(request, 'about.html')
+
+def bienvenido(request):
+    private_flans = Flan.objects.filter(is_private=True)
+    context = {
+        'private_flans': private_flans
+    }
+
+    return render(request, 'welcome.html', context)
+
+def contacto(request):
+    if request.method == 'POST':
+        form = ContactFormForm(request.POST)
+
+        if form.is_valid():
+
+            contact_form = ContactForm.objects.create(**form.cleaned_data)
+            return HttpResponseRedirect ('/exito')
+    else:
+        form = ContactFormForm()    
+    context = {'form': form}
+
+    return render(request, 'contact.html', context)    
+
+
+def exito(request):
+    return render(request, 'exito.html')
+
+
+    
