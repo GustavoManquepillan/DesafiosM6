1458ea51cd4dd2921bbe46c9304caf588d6325b5
Gustavo Manquepillán <gustavomanquepillan@gmail.com>
2024-05-24 00:02:29 -0400

    Subiendo Hito 4 a github

--- Proyectos/Proyecto/onlyflans/web/views.py
+++ Proyectos/Proyecto/onlyflans/web/views.py
@@ -1,36 +1,32 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 from .models import Flan
-from .forms import  ContactFormModelForm
-
+from .forms import ContactFormModelForm
 
 def indice(request):
-    #el contexto, es el diccionario donde se envian los datos
-    # flanes = Flan.objects.all() #SELECT * FROM FLAN WHERE is_private=False
     public_flans = Flan.objects.filter(is_private=False)
     context = {
         'public_flans':public_flans
     }
     return render(request, 'index.html', context)
 
-
 def acerca(request):
     return render(request, 'about.html', {})
 
-
+@login_required
 def bienvenido(request):
-    private_flans = Flan.objects.filter(is_private=True) #ORM Django Flan(tabla) objects filter, is_private=true
-    #SELECT * FROM FLan WHERE is_private=True
+    private_flans = Flan.objects.filter(is_private=True)
     context = {
         'private_flans': private_flans
     }
     return render(request, 'welcome.html', context)
 
-
 def contacto(request):
     if request.method == 'POST':
         form = ContactFormModelForm(request.POST)
         if form.is_valid():
-            # Guardar el formulario en la base de datos
             contact_form_instance = form.save()
             return HttpResponseRedirect('/exito/')
     else:
@@ -38,8 +34,24 @@ def contacto(request):
     context = {'form': form}
     return render(request, 'contact.html', context)
 
-
- 
 def exito(request):
     return render(request, 'exito.html')
+
+class MiVistaProtegida(LoginRequiredMixin, TemplateView):
+    template_name = 'about.html'
+
+def detalle_receta(request, nombre):
+    # Lógica para obtener los detalles de la receta con el nombre proporcionado
+    # Utiliza el nombre de la receta para obtener los detalles necesarios
+    # Por ejemplo:
+    try:
+        flan = Flan.objects.get(slug=nombre)
+        # Ahora puedes usar 'flan' para obtener los detalles de la receta
+        return render(request, 'detalle_receta.html', {'flan': flan})
+    except Flan.DoesNotExist:
+        # Maneja el caso donde la receta no existe
+        return HttpResponseNotFound("La receta no existe")
+
+
+
 
